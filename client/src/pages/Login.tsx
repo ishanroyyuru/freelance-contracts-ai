79b9cd59ee36ed91bcf14639429d247c868473b3
Ishan Royyuru <142559396+ishanroyyuru@users.noreply.github.com>
2025-07-05 00:03:57 -0700

    Implemented login page frontend

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,55 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { useAuth } from "../contexts/AuthContext";
+
 export default function Login() {
-  return <h1 className="text-2xl">Login Page</h1>;
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { setToken } = useAuth();
+    const navigate = useNavigate();
+    
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try{
+            const res = await axios.post("http://localhost:5001/login", { email, password });
+            setToken(res.data.token);
+            navigate("/", { replace: true});
+        } catch(err: any){
+            alert(err.response?.data?.error || "Login failed");
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="max-w-sm mx-auto mt-24 space-y-4">
+            <h1 className="text-2xl font-bold">Log in</h1>
+            <input
+                type="email"
+                placeholder="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                className="w-full border p-2"
+            />
+
+            <input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className="w-full border p-2"
+            />
+
+            <button type="submit" className="w-full bg-blue-600 text-white py-2 rounded">
+                Log in
+            </button>
+            
+            <p>
+                Don't have an account?{" "}
+                <Link to="/signup" className="text-blue-600">
+                    Sign up
+                </Link>
+            </p>
+        </form>
+    );
+};
